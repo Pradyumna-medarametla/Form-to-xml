6a37dac48bc2b5af8b3cbe65878953592e560adb
Pradyumna <medarametla@st.niituniversity.in>
2020-08-17 21:26:33 +0530

    Delete FolderTraversal.java

--- FolderTraversal.java
+++
@@ -1,8 +0,0 @@
-File folder = new File("/Users/you/folder/");
-File[] listOfFiles = folder.listFiles();
-
-for (File file : listOfFiles) {
-    if (file.isFile()) {
-        System.out.println(file.getName());
-    }
-}
